606151cb598a30aa81420e9aed7af2471899fc61
CODERBLOXx <78531418+CODERBLOXx@users.noreply.github.com>
2021-07-02 18:00:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX = 0;
+noseY = 0;
+
 function preload(){
 }
 
@@ -7,8 +10,20 @@ function setup(){
 
     canvas = createCanvas(550,500);
     canvas.position(560,150);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     background('#969A97');
+}
+
+function modelLoaded(){
+console.log('PoseNet is initialized');
+}
+
+function gotPoses(results){
+if(results.length > 0){
+    console.log(results);
+}
 }
